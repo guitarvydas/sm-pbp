62dc3d57d4ba8b5ba41fbb629b32cbd6e4a3e89d
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-04-16 13:39:54 -0400

    allow mergeCode.pl to fail to find code to merge

--- out.py
+++ out.py
@@ -44,6 +44,64 @@ class SM_✣:
     def exit_wait_for_min_recrossing (self):
         e = self.env
         pass
+    def enter_idle (self):
+        e = self.env
+        self.state = "idle"
+        e.update (" >> entering 'idle'")
+        
+    def step_idle (self):
+        e = self.env
+        if (e.x <= e.xmin):
+            self.exit_idle ()
+            e.scoreRight ()
+            self.enter_idle ()
+    def exit_idle (self):
+        e = self.env
+        pass
+    def enter_left_paddle (self):
+        e = self.env
+        self.state = "left_paddle"
+        e.update (" >> entering 'left_paddle'")
+        
+    def step_left_paddle (self):
+        e = self.env
+        
+    def exit_left_paddle (self):
+        e = self.env
+        pass
+    def enter_right_paddle (self):
+        e = self.env
+        self.state = "right_paddle"
+        e.update (" >> entering 'right_paddle'")
+        
+    def step_right_paddle (self):
+        e = self.env
+        
+    def exit_right_paddle (self):
+        e = self.env
+        pass
+    def enter_ceiling (self):
+        e = self.env
+        self.state = "ceiling"
+        e.update (" >> entering 'ceiling'")
+        
+    def step_ceiling (self):
+        e = self.env
+        
+    def exit_ceiling (self):
+        e = self.env
+        pass
+    def enter_floor (self):
+        e = self.env
+        self.state = "floor"
+        e.update (" >> entering 'floor'")
+        
+    def step_floor (self):
+        e = self.env
+        
+    def exit_floor (self):
+        e = self.env
+        pass
     def __init__ (self, env):
         self.env = env
         self.state = None
@@ -53,4 +111,9 @@ class SM_✣:
             "idle": self.step_idle,
             "wait for max recrossing": self.step_wait_for_max_recrossing,
             "wait for min recrossing": self.step_wait_for_min_recrossing,
+            "idle": self.step_idle,
+            "left_paddle": self.step_left_paddle,
+            "right_paddle": self.step_right_paddle,
+            "ceiling": self.step_ceiling,
+            "floor": self.step_floor,
         } [self.state] ()
